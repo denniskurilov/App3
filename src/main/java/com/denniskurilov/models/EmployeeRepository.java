894522dd3c9143d95c7ae5aa4cf5ad61b10bffed
denniskurilov <denniskurilov@gmail.com>
2023-05-06 14:53:25 +0200

    init

---
+++ src/main/java/com/denniskurilov/models/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.denniskurilov.models;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
+
---
+++ src/main/java/com/denniskurilov/models/EmployeeService.java
@@ -0,0 +1,34 @@
+package com.denniskurilov.models;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class EmployeeService {
+    @Autowired EmployeeRepository repo;
+    
+    public void save(Employee employee) {
+        repo.save(employee);
+    }
+     
+    public List<Employee> getAll() {
+        return (List<Employee>) repo.findAll(Sort.by(Sort.Direction.ASC, "id"));
+    }
+     
+    public Employee get(long id) {
+        return repo.findById(id).get();
+    }
+     
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
+     
+    
+    
+    
+}
